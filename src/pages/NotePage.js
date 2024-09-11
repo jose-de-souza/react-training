08774f6f0a0211b2e64a88b7441786a28d0b4c33
José Esteves de Souza Neto <email@jose-desouza.com>
2024-09-11 11:39:37 +0200

    added create, update, delete

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -4,7 +4,7 @@ import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 //import notes from "../assets/data";
 
-function NotePage({ match }) {
+function NotePage({ match, history }) {
   let noteId = match.params.id;
   //let note = notes.find((note) => note.id == noteId);
 
@@ -15,9 +15,53 @@ function NotePage({ match }) {
   }, [noteId]);
 
   let getNote = async () => {
+    if (noteId === "new") return;
     let response = await fetch(`http://localhost:5000/notes/${noteId}`);
     let data = await response.json();
     setNote(data);
+  };
+
+  let createNote = async () => {
+    await fetch(`http://localhost:5000/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let updateNote = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let deleteNote = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    history.push("/");
+  };
+
+  let handleSubmit = () => {
+    if (noteId != "new" && !note.body) {
+      deleteNote();
+    } else if (noteId !== "new") {
+      updateNote();
+    } else if (noteId === "new" && note !== null) {
+      createNote();
+    }
+
+    history.push("/");
   };
 
   return (
@@ -25,12 +69,23 @@ function NotePage({ match }) {
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+
+        {noteId !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
       </div>
 
-      <textarea value={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 }
